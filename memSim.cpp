b23827603b6fa6ef628e5d0fac95d04f0ec1e123
luke-plewa <luke.plewa@gmail.com>
2014-02-19 11:20:42 -0800

    add header

---
+++ memSim.cpp
@@ -0,0 +1,1 @@
+#include "memSim.h"
---
+++ memSim.h
@@ -0,0 +1,24 @@
+class TLB {
+  private:
+  public:
+};
+
+class PageTable {
+  private:
+  public:
+};
+
+class Disk {
+  private:
+  public:
+};
+
+class PhysMem {
+  private:
+  public:
+};
+
+class RefSeq {
+  private:
+  public:
+};
